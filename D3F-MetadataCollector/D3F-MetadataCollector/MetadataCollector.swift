79d4ca3d4f411361a97a1eda2f6d74dd19bf3880
Max-Joseph Krempl <krempl@in.tum.de>
2020-09-22 01:26:45 +0200

    Added MusicBrainz disc ID parsing

--- D3F-MetadataCollector/D3F-MetadataCollector/MetadataCollector.swift
+++ D3F-MetadataCollector/D3F-MetadataCollector/MetadataCollector.swift
@@ -15,6 +15,7 @@ class MetadataCollector {
 		case csv = "csv"
 		case ffmetadata = "ffmetadata"
 		case dataDir = "dataDir"
+		case mbDiscIDList = "mbDiscIDList"
 	}
 	enum CollectionType: String {
 		case serie = "serie"
@@ -192,7 +193,7 @@ class MetadataCollector {
 						for (i, line) in content.dropFirst().enumerated() {
 							let lineNumber = i+2
 							do {
-								try handleCSV(line: line, overwrite: overwrite)
+								try handleCSV(line: line, collectionType: collectionType, overwrite: overwrite)
 							}
 							catch let error as CSVParseError {
 								stderr("Error parsing csv file \"\(url.path)\" at line \(lineNumber):  \(error.description)")
@@ -205,6 +206,18 @@ class MetadataCollector {
 						}
 						catch let error as FFMetadataParseError {
 							stderr("Error parsing data directory \"\(url.path)\":  \(error.description)")
+						}
+					
+					case .mbDiscIDList:
+						let content = try String(contentsOf: url).split(separator: "\n")
+						for (i, line) in content.enumerated() {
+							let lineNumber = i+1
+							do {
+								try handleMBDiscIDList(line: line, collectionType: collectionType, overwrite: overwrite)
+							}
+							catch let error as MBDiscIDListParseError {
+								stderr("Error parsing MusicBrainz discID list file \"\(url.path)\" at line \(lineNumber):  \(error.description)")
+							}
 						}
 				}
 			}
@@ -371,7 +384,9 @@ class MetadataCollector {
 		}
 	}
 	
-	func handleCSV<T: StringProtocol>(line: T, overwrite: Bool) throws {
+	func handleCSV<T: StringProtocol>(line: T, collectionType: CollectionType, overwrite: Bool) throws {
+		guard collectionType == .serie else { return }
+		
 		guard let startIdentifierIndex = line.firstIndex(of: "["), let endIndex = line.lastIndex(of: "]") else {
 			throw CSVParseError.invalidCSVDataSet
 		}
@@ -569,6 +584,164 @@ class MetadataCollector {
 		
 		let links = try parseDirectoryContents(url: directory)
 		höreinheit.links = links
+	}
+	
+	
+	enum MBDiscIDListParseError: Error, CustomStringConvertible {
+		case invalidURL(urlString: String)
+		case requestFailed(errorMsg: String)
+		case jsonParsingFailed(errorMsg: String)
+		case invalidTitleFormat
+		case unexpectedTrackSequence(expected: Int, actual: Int)
+		case trackAndOffsetCountDiffer(tracks: UInt, offsets: UInt)
+		case zeroTracks
+		case negativeTimestamp(timestamp: Int)
+		
+		var description: String {
+			switch self {
+				case .invalidURL(let urlString): return "Invalid URL:  \(urlString)"
+				case .requestFailed(let errorMsg): return "Request to MusicBrainz failed:  \(errorMsg)"
+				case .jsonParsingFailed(let errorMsg): return "Parsing MusicBrainz JSON response failed:  \(errorMsg)"
+				case .invalidTitleFormat: return "Title value has an unexpected format; \"nummer\" couldn't be parsed"
+				case .unexpectedTrackSequence(let expected, let actual): return "Expected next track position to be \(expected) but was \(actual)"
+				case .trackAndOffsetCountDiffer(let tracks, let offsets): return "Number of tracks (\(tracks) and number of offsets (\(offsets) don't match"
+				case .zeroTracks: return "No tracks were found"
+				case .negativeTimestamp(let timestamp): return "Negative timestamp (\(timestamp) after conversion"
+			}
+		}
+	}
+	
+	func handleMBDiscIDList<T: StringProtocol>(line: T, collectionType: CollectionType, overwrite: Bool) throws {
+		guard collectionType == .serie else { return }
+		
+		func synchronousJSONRequest(to urlString: String) throws -> Any {
+			guard let url = URL(string: urlString) else {
+				throw MBDiscIDListParseError.invalidURL(urlString: urlString)
+			}
+			var result: Any?
+			var errorToThrow: Error?
+			let semphore = DispatchSemaphore(value: 0)
+			let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+				defer {
+					semphore.signal()
+				}
+				if let error = error {
+					errorToThrow = MBDiscIDListParseError.requestFailed(errorMsg: error.localizedDescription)
+					return
+				}
+				guard let data = data else {
+					errorToThrow = MBDiscIDListParseError.requestFailed(errorMsg: "No data")
+					return
+				}
+				let httpResponse = response as! HTTPURLResponse
+				guard httpResponse.statusCode == 200 else {
+					errorToThrow = MBDiscIDListParseError.requestFailed(errorMsg: "\(httpResponse.statusCode): \(String(data: data, encoding: .utf8) ?? "(nil)")")
+					return
+				}
+				
+				do {
+					result = try JSONSerialization.jsonObject(with: data, options: [])
+				}
+				catch(let parseError) {
+					errorToThrow = MBDiscIDListParseError.jsonParsingFailed(errorMsg: parseError.localizedDescription)
+					return
+				}
+			}
+			task.resume()
+			semphore.wait()
+			usleep(1_000_000)  // Wait 1000ms, to keep MusicBrainz' servers happy (https://wiki.musicbrainz.org/MusicBrainz_API/Rate_Limiting)
+			
+			if let error = errorToThrow {
+				throw error
+			}
+			return result!
+		}
+		
+		// Get disc data
+		let discID = line.trimmingCharacters(in: .whitespaces)
+		guard let disc = try synchronousJSONRequest(to: "https://musicbrainz.org/ws/2/discid/\(discID)?fmt=json") as? [String: Any] else {
+			throw MBDiscIDListParseError.jsonParsingFailed(errorMsg: "Disc top level not a dictionary")
+		}
+		guard let discRelease = (disc["releases"] as? [[String: Any]])?.first else {
+			throw MBDiscIDListParseError.jsonParsingFailed(errorMsg: "Release object not found")
+		}
+		guard let sectorOffsets = disc["offsets"] as? [Int], let sectorCount = disc["sectors"] as? Int else {
+			throw MBDiscIDListParseError.jsonParsingFailed(errorMsg: "Sector data not found")
+		}
+		guard let releaseID = discRelease["id"] as? String else {
+			throw MBDiscIDListParseError.jsonParsingFailed(errorMsg: "Release ID not found")
+		}
+		
+		// Get release data
+		guard let release = try synchronousJSONRequest(to: "https://musicbrainz.org/ws/2/release/\(releaseID)?inc=recordings&fmt=json") as? [String: Any] else {
+			throw MBDiscIDListParseError.jsonParsingFailed(errorMsg: "Release top level not a dictionary")
+		}
+		guard let medium = (release["media"] as? [[String: Any]])?.first else {
+			throw MBDiscIDListParseError.jsonParsingFailed(errorMsg: "Medium not found")
+		}
+		guard let tracks = medium["tracks"] as? [[String: Any]] else {
+			throw MBDiscIDListParseError.jsonParsingFailed(errorMsg: "Tracks not found")
+		}
+		guard let title = release["title"] as? String else {
+			throw MBDiscIDListParseError.jsonParsingFailed(errorMsg: "Title not found")
+		}
+		
+		// Parse nummer
+		guard let nummerEndIndex = title.firstIndex(of: ":"), let nummerIdentifierIndex = title[..<nummerEndIndex].lastIndex(of: " ") else {
+			throw MBDiscIDListParseError.invalidTitleFormat
+		}
+		let nummerStartIndex = title.index(after: nummerIdentifierIndex)
+		let nummerString = title[nummerStartIndex..<nummerEndIndex]
+		guard let nummer = UInt(nummerString) else {
+			throw MBDiscIDListParseError.invalidTitleFormat
+		}
+		
+		// Generate chapters
+		var previousPosition = 0
+		let kapitels: [Kapitel] = try tracks.map { track in
+			guard let position = track["position"] as? Int else {
+				throw MBDiscIDListParseError.jsonParsingFailed(errorMsg: "Track position not found")
+			}
+			let expectedPosition = previousPosition + 1
+			guard position == expectedPosition else {
+				throw MBDiscIDListParseError.unexpectedTrackSequence(expected: expectedPosition, actual: position)
+			}
+			previousPosition = expectedPosition
+			
+			guard let trackTitle = (track["title"] as? String)?.trimmingCharacters(in: .whitespaces) else {
+				throw MBDiscIDListParseError.jsonParsingFailed(errorMsg: "Track title not found")
+			}
+			return Kapitel(titel: trackTitle)
+		}
+		
+		guard kapitels.count == sectorOffsets.count else {
+			throw MBDiscIDListParseError.trackAndOffsetCountDiffer(tracks: UInt(kapitels.count), offsets: UInt(sectorOffsets.count))
+		}
+		guard kapitels.count > 0 else {
+			throw MBDiscIDListParseError.zeroTracks
+		}
+		
+		let startOffset = sectorOffsets.first!
+		let timestamps: [Int] = try (sectorOffsets + [sectorCount]).map { 
+			let sectors = $0 - startOffset
+			let samples = sectors * 2352 / 4  // sectorSize=2352B ; sampleSize=2*16bit=4B
+			let milliseconds = Int((Double(samples) / 44.1).rounded())  // sampleRate=44.1kHz
+			guard milliseconds >= 0 else {
+				throw MBDiscIDListParseError.negativeTimestamp(timestamp: milliseconds)
+			}
+			return Int(milliseconds)
+		}
+		for (i, (start, end)) in zip(timestamps.dropLast(), timestamps.dropFirst()).enumerated() {
+			let kapitel = kapitels[i]
+			kapitel.start = start
+			kapitel.end = end
+		}
+		
+		// Find or create folge based on nummer & update chapters
+		let folge = findOrCreateFolge(nummer: nummer, in: &metadata.serie!)
+		if folge.kapitel == nil {
+			folge.kapitel = kapitels
+		}
 	}
 	
 	
