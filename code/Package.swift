669f2164a4c75baa0c8b73f124352592c00a5b6d
YourMJK <developer@mjkrempl.com>
2024-06-26 15:50:17 +0200

    code: Added `GRDB.swift` package dependency

--- code/Package.swift
+++ code/Package.swift
@@ -10,6 +10,7 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/YourMJK/CommandLineTool", from: "1.1.0"),
 		.package(url: "https://github.com/dehesa/CodableCSV", from: "0.6.0"),
+		.package(url: "https://github.com/groue/GRDB.swift", from: "6.27.0"),
 	],
 	targets: [
 		.executableTarget(
@@ -17,6 +18,7 @@ let package = Package(
 			dependencies: [
 				"CommandLineTool",
 				"CodableCSV",
+				.product(name: "GRDB", package: "GRDB.swift"),
 			],
 			path: "dreimetadaten"
 		)
