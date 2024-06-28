ca04abc7037d5a759080f1a0c2ae59334f63e03e
YourMJK <developer@mjkrempl.com>
2024-06-28 03:30:46 +0200

    code: Added safety fail to `load` subcommand if database already exists
    along with option to overwrite it

--- code/dreimetadaten/Command/Command.Load.swift
+++ code/dreimetadaten/Command/Command.Load.swift
@@ -27,13 +27,44 @@ extension Command {
 		@Option(name: .customLong("sqlite"), help: ArgumentHelp("The path to the SQLite CLI binary.", valueName: "sqlite binary"))
 		var sqliteBinaryPath: String = SQLPorter.defaultSqliteBinaryPath
 		
+		@Flag(name: .shortAndLong, help: ArgumentHelp("Overwrite database file if it already exists."))
+		var force: Bool = false
+		
 		func run() throws {
 			let databaseFile = URL(fileURLWithPath: databaseFilePath, isDirectory: false)
 			let sqlFile = URL(fileURLWithPath: sqlFilePath, isDirectory: false)
 			let sqliteBinary = URL(fileURLWithPath: sqliteBinaryPath, isDirectory: false)
+			
+			if FileManager.default.fileExists(atPath: databaseFile.path) {
+				guard force else {
+					throw ArgumentsError.databaseExists(url: databaseFile)
+				}
+				// Clear existing database file
+				do {
+					try Data().write(to: databaseFile)
+				}
+				catch {
+					throw SQLPorter.IOError.fileWritingFailed(url: databaseFile, error: error)
+				}
+			}
 			
 			let porter = SQLPorter(databaseFile: databaseFile, sqliteBinary: sqliteBinary)
 			try porter.import(from: sqlFile)
 		}
 	}
 }
+
+
+extension Command.Load {
+	enum ArgumentsError: LocalizedError {
+		case databaseExists(url: URL)
+		
+		var errorDescription: String? {
+			switch self {
+				case .databaseExists(let url):
+					return "Database file \"\(url.relativePath)\" already exists. Use option -f to overwrite it."
+			}
+		}
+	}
+}
+
