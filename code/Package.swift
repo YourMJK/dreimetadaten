6e1487b250af2e33560a960445a22088465eff2e
YourMJK <developer@mjkrempl.com>
2024-07-06 12:19:02 +0200

    code: Added `swift-collections.git` package dependency & Specified minimum platform version

--- code/Package.swift
+++ code/Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
 	name: "dreimetadaten",
+	platforms: [.macOS(.v10_15)],
 	products: [
 		.executable(name: "dreimetadaten", targets: ["dreimetadaten"]),
 	],
@@ -11,6 +12,7 @@ let package = Package(
 		.package(url: "https://github.com/YourMJK/CommandLineTool", from: "1.1.0"),
 		.package(url: "https://github.com/dehesa/CodableCSV", from: "0.6.0"),
 		.package(url: "https://github.com/groue/GRDB.swift", from: "6.27.0"),
+		.package(url: "https://github.com/apple/swift-collections.git", from: "1.1.0"),
 	],
 	targets: [
 		.executableTarget(
@@ -19,6 +21,7 @@ let package = Package(
 				"CommandLineTool",
 				"CodableCSV",
 				.product(name: "GRDB", package: "GRDB.swift"),
+				.product(name: "Collections", package: "swift-collections")
 			],
 			path: "dreimetadaten"
 		)
