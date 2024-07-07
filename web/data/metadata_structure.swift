0aa147de50d6d1b4fbd85db8dc6f4937d06cf7fc
YourMJK <developer@mjkrempl.com>
2024-07-07 14:31:07 +0200

    Exported object model generated from database as web data directory
    and fixed some minor issues in previous web data

--- web/data/metadata_structure.swift
+++ web/data/metadata_structure.swift
@@ -1,50 +1,57 @@
-struct Metadata {
+struct MetadataObjectModel {
 	var serie: [Folge]?
-	var spezial: [Höreinheit]?
-	var kurzgeschichten: [Höreinheit]?
+	var spezial: [Hörspiel]?
+	var kurzgeschichten: [Hörspiel]?
 	var die_dr3i: [Folge]?
 }
 
 
-class Höreinheit {
+class Hörspiel {
 	var teile: [Teil]?
 	var titel: String?
 	var autor: String?
 	var hörspielskriptautor: String?
+	var kurzbeschreibung: String?
 	var beschreibung: String?
+	var metabeschreibung: String?
 	var veröffentlichungsdatum: String?
 	var kapitel: [Kapitel]?
 	var sprechrollen: [Sprechrolle]?
+	var medien: [Medium]?
 	var links: Links?
 	var unvollständig: Bool?
 }
 
-class Folge: Höreinheit {
-	let nummer: Int
+class Folge: Hörspiel {
+	var nummer: Int
 }
 
-class Teil: Höreinheit {
-	let teilNummer: UInt
+class Teil: Hörspiel {
+	var teilNummer: UInt
 	var buchstabe: String?
 }
 
 
-class Kapitel {
-	let titel: String
+struct Kapitel {
+	var titel: String
 	var start: Int?
 	var end: Int?
 }
 
-class Sprechrolle {
+struct Sprechrolle {
 	var rolle: String
 	var sprecher: String
 	var pseudonym: String?
 }
 
-class Links {
+struct Medium {
+	var tracks: [Kapitel]
+	var xld_log: String?
+}
+
+struct Links {
 	var json: String?
 	var ffmetadata: String?
-	var xld_log: String?
 	var cover: String?
 	var cover_itunes: String?
 	var cover_kosmos: String?
