230448a8f97607285ab3bfcfba3e7f138395e72a
YourMJK <developer@mjkrempl.com>
2024-07-22 00:16:24 +0200

    code: Fixed overwriting behavior in tools/loadDB.sh

--- tools/loadDB.sh
+++ tools/loadDB.sh
@@ -2,4 +2,16 @@
 
 source $(dirname "$0")/.config
 
+if [ -f "$DBFILE" ]
+then
+	if [ "$1" = "-f" ]
+	then
+		rm "$DBFILE"
+	else
+		echo "Database file already exists. Type \"y\" to confirm. Use flag \"-f\" to overwrite it without warning."
+		rm -i "$DBFILE"
+	fi
+	[ -f "$DBFILE" ] && exit 1
+fi
+
 sqlite3 "$DBFILE" < "$SQLFILE"
