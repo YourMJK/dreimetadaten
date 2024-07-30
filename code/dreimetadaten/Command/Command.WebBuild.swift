f77054b531137903633456d83b0608c50c2c65b0
YourMJK <developer@mjkrempl.com>
2024-07-30 11:55:57 +0200

    code: Added "webbuild all" subcommand shortcut

--- code/dreimetadaten/Command/Command.WebBuild.swift
+++ code/dreimetadaten/Command/Command.WebBuild.swift
@@ -8,6 +8,7 @@
 import Foundation
 import CommandLineTool
 import ArgumentParser
+import GRDB
 
 
 extension Command {
@@ -21,9 +22,6 @@ extension Command {
 		struct IOOptions: ParsableArguments {
 			private static let argumentHelpAutomaticDefault = "(default: automatic based on <collection type>)"
 			
-			@Option(name: .customLong("json"), help: ArgumentHelp("The path to the JSON dataset input file. \(argumentHelpAutomaticDefault)", valueName: "json file"))
-			var jsonFilePath: String?
-			
 			@Option(name: .customLong("template"), help: ArgumentHelp("The path to the template HTML input file to use for filling in the placeholders. \(argumentHelpAutomaticDefault)", valueName: "html file"))
 			var templateFilePath: String?
 			
@@ -32,50 +30,64 @@ extension Command {
 		}
 		
 		@Argument(help: ArgumentHelp("The collection type to generate the HTML code for.", valueName: "collection type"))
-		var collectionType: CollectionType
+		var collectionTypeArgument: CollectionTypeArgument
+		
+		@Option(name: .customLong("db"), help: ArgumentHelp("The path to the SQLite database file.", valueName: "sqlite file"))
+		var databaseFilePath: String = Command.databaseFile.relativePath
+		
+		@Option(name: .customLong("webDataURL"), help: ArgumentHelp("The URL pointing to the web data directory. Used as the base URL for generated metadata links.", valueName: "URL"))
+		var webDataURLString: String = Command.webDataURL.absoluteString
 		
 		@OptionGroup(title: "IO Options")
 		var ioOptions: IOOptions
 		
 		func run() throws {
-			func automaticDefault(_ optionKeyPath: KeyPath<IOOptions, String?>, defaultFile defaultFileKeyPath: KeyPath<CollectionType, String>, in defaultDir: URL) throws -> URL {
-				let url = ioOptions[keyPath: optionKeyPath]
-					.map { URL(fileURLWithPath: $0, isDirectory: false) }
-					?? defaultDir.appendingPathComponent(collectionType[keyPath: defaultFileKeyPath], isDirectory: false)
+			guard let webDataURL = URL(string: webDataURLString) else {
+				throw IOError.invalidURL(string: webDataURLString)
+			}
+			
+			let dbQueue = try DatabaseQueue(path: databaseFilePath)
+			try dbQueue.read { db in
+				let objectModel = try MetadataObjectModel(fromDatabase: db, withBaseURL: webDataURL)
 				
-				var directory: ObjCBool = false
-				guard FileManager.default.fileExists(atPath: url.path, isDirectory: &directory), !directory.boolValue else {
-					throw IOError.noSuchFile(url: url)
+				for collectionType in collectionTypeArgument.collectionType {
+					func automaticDefault(_ optionKeyPath: KeyPath<IOOptions, String?>, defaultIn defaultDir: URL) throws -> URL {
+						let path = ioOptions[keyPath: optionKeyPath]
+						let defaultFile = collectionType.htmlFile
+						let url = path.map { URL(fileURLWithPath: $0, isDirectory: false) } ?? defaultDir.appendingPathComponent(defaultFile, isDirectory: false)
+						
+						var directory: ObjCBool = false
+						guard FileManager.default.fileExists(atPath: url.path, isDirectory: &directory), !directory.boolValue else {
+							throw IOError.noSuchFile(url: url)
+						}
+						return url
+					}
+					let templateFileURL = try automaticDefault(\.templateFilePath, defaultIn: Command.webTemplatesDir)
+					let outputFileURL = try automaticDefault(\.outputFilePath, defaultIn: Command.webDir)
+					
+					let templateContent: String
+					do {
+						templateContent = try String(contentsOf: templateFileURL, encoding: .utf8)
+					}
+					catch {
+						throw IOError.fileReadingFailed(url: templateFileURL, error: error)
+					}
+					
+					let webBuilder = WebBuilder(
+						objectModel: objectModel,
+						collectionType: collectionType,
+						templateContent: templateContent,
+						host: webDataURL.host!
+					)
+					try webBuilder.build()
+					
+					do {
+						try webBuilder.content.write(to: outputFileURL, atomically: false, encoding: .utf8)
+					}
+					catch {
+						throw IOError.fileWritingFailed(url: outputFileURL, error: error)
+					}
 				}
-				return url
-			}
-			
-			let jsonFileURL = try automaticDefault(\.jsonFilePath, defaultFile: \.jsonFile, in: Command.jsonDir)
-			let templateFileURL = try automaticDefault(\.templateFilePath, defaultFile: \.htmlFile, in: Command.webTemplatesDir)
-			let outputFileURL = try automaticDefault(\.outputFilePath, defaultFile: \.htmlFile, in: Command.webDir)
-			
-			let templateContent: String
-			do {
-				templateContent = try String(contentsOf: templateFileURL, encoding: .utf8)
-			}
-			catch {
-				throw IOError.fileReadingFailed(url: templateFileURL, error: error)
-			}
-			
-			let objectModel = try MetadataObjectModel(fromJSON: jsonFileURL)
-			let webBuilder = WebBuilder(
-				objectModel: objectModel,
-				collectionType: collectionType,
-				templateContent: templateContent,
-				host: Command.webURL.host!
-			)
-			try webBuilder.build()
-			
-			do {
-				try webBuilder.content.write(to: outputFileURL, atomically: false, encoding: .utf8)
-			}
-			catch {
-				throw IOError.fileWritingFailed(url: outputFileURL, error: error)
 			}
 		}
 	}
@@ -83,10 +95,29 @@ extension Command {
 
 
 extension Command.WebBuild {
+	enum CollectionTypeArgument: String, ArgumentEnum {
+		case serie
+		case spezial
+		case kurzgeschichten
+		case die_dr3i
+		case all
+		
+		var collectionType: [CollectionType] {
+			switch self {
+				case .serie: return [.serie]
+				case .spezial: return [.spezial]
+				case .kurzgeschichten: return [.kurzgeschichten]
+				case .die_dr3i: return [.die_dr3i]
+				case .all: return CollectionType.allCases
+			}
+		}
+	}
+	
 	enum IOError: LocalizedError {
 		case noSuchFile(url: URL)
 		case fileReadingFailed(url: URL, error: Error)
 		case fileWritingFailed(url: URL, error: Error)
+		case invalidURL(string: String)
 		
 		var errorDescription: String? {
 			switch self {
@@ -96,6 +127,8 @@ extension Command.WebBuild {
 					return "Couldn't read file \"\(url.relativePath)\": \(error.localizedDescription)"
 				case .fileWritingFailed(let url, let error):
 					return "Couldn't write to file \"\(url.relativePath)\": \(error.localizedDescription)"
+				case .invalidURL(let string):
+					return "Invalid URL \"\(string)\""
 			}
 		}
 	}
